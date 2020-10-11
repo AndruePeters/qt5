9eb0360f17c19417ef46b4c7d95ccb47f9e56cab
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-10-11 01:01:33 -0400

    MVC

--- cm-lib/src/controllers/main-controller.h
+++ cm-lib/src/controllers/main-controller.h
@@ -1,7 +1,10 @@
 #ifndef MAINCONTROLLER_H
 #define MAINCONTROLLER_H
 
+#include <string>
+
 #include <QObject>
+#include <QString>
 
 namespace cm::controllers {
 
@@ -9,9 +12,14 @@ namespace cm::controllers {
 class Q_DECL_EXPORT MainController : public QObject {
 
 Q_OBJECT
+    Q_PROPERTY( QString ui_welcomeMessage READ welcomeMessageSTLConv  )
 
 public:
     explicit MainController(QObject *parent = nullptr);
+    std::string welcomeMessage = "This is the MainController";
+
+    QString welcomeMessageSTLConv() const { return QString::fromStdString(welcomeMessage); }
+    QString asf = "test message";
 
 signals:
 
--- cm-ui/src/main.cpp
+++ cm-ui/src/main.cpp
@@ -1,12 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include <controllers/main-controller.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    qmlRegisterType<cm::controllers::MainController>("CM", 1, 0, "MainController");
+    cm::controllers::MainController mainController;
+
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/views/MainWindow.qml"));
+    engine.rootContext()->setContextProperty("mainController", &mainController);
+    const QUrl url(QStringLiteral("qrc:/views/MainView"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
