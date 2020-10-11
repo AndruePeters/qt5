0cd3504ef527b4e696ea0130427d95240d9d8eb1
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-10-10 22:04:12 -0400

    Updated views path

--- cm-ui/src/main.cpp
+++ cm-ui/src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/views/main.qml"));
+    const QUrl url(QStringLiteral("qrc:/views/MainWindow.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
