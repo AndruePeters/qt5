24108253d2dd6cdb02f77591155000eac6c4debc
AndruePeters <druepetes@gmail.com>
2020-10-12 10:30:00 -0400

    Added welcomeMessage

--- cm-lib/src/controllers/main-controller.h
+++ cm-lib/src/controllers/main-controller.h
@@ -1,25 +1,18 @@
 #ifndef MAINCONTROLLER_H
 #define MAINCONTROLLER_H
 
-#include <string>
-
 #include <QObject>
 #include <QString>
 
 namespace cm::controllers {
-
-
 class Q_DECL_EXPORT MainController : public QObject {
 
 Q_OBJECT
-    Q_PROPERTY( QString ui_welcomeMessage READ welcomeMessageSTLConv  )
+    Q_PROPERTY( QString ui_welcomeMessage MEMBER welcomeMessage CONSTANT )
 
 public:
     explicit MainController(QObject *parent = nullptr);
-    std::string welcomeMessage = "This is the MainController";
-
-    QString welcomeMessageSTLConv() const { return QString::fromStdString(welcomeMessage); }
-    QString asf = "test message";
+    QString welcomeMessage = "This is the MainController";
 
 signals:
 
